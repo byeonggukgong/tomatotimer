4070423eb6a02e42c7a8f5ba4049668e54474c8b
Byeongguk Gong <byeonggukgong@gmail.com>
2019-05-04 15:31:34 +0900

    Bootstrap 4 추가

---
+++ src/main/java/com/example/tomatotimer/web/IndexController.java
@@ -0,0 +1,11 @@
+package com.example.tomatotimer.web;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class IndexController {
+
+    public String index() {
+        return "index";
+    }
+}
