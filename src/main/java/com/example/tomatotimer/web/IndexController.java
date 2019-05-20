e43a5be6fa155de35156544391f1c2b16b199cb2
Byeongguk Gong <byeonggukgong@gmail.com>
2019-05-20 23:15:44 +0900

    User 인증 상태별 버튼 추가

--- src/main/java/com/example/tomatotimer/web/IndexController.java
+++ src/main/java/com/example/tomatotimer/web/IndexController.java
@@ -2,14 +2,19 @@ package com.example.tomatotimer.web;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.security.Principal;
 
 @Slf4j
 @Controller
 public class IndexController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model, Principal principal) {
+        model.addAttribute("isAuthenticated", principal);
+
         return "index";
     }
 }
